c03aeeff43f06eda08af30d2ee9565c7b2da7743
Meet Jethwa <meetjethwa3@gmail.com>
2024-05-24 22:13:55 +0530

    Update setup.sh

--- projects/setup.sh
+++ projects/setup.sh
@@ -4,5 +4,9 @@
 apt-get update -y
 apt-get install -y openjdk-11-jdk
 
+# Set JAVA_HOME environment variable
+export JAVA_HOME=/usr/lib/jvm/java-11-openjdk-amd64
+export PATH=$JAVA_HOME/bin:$PATH
+
 # Verify Java installation
 java -version
